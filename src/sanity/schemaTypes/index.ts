1378c9a8593d437975f46d48d9c5e785047b32d6
Oluwasomidotun <victoroluwasomidotun@gmail.com>
2024-11-04 22:01:32 +0100

    sanity schema done and connected

---
+++ src/sanity/schemaTypes/index.ts
@@ -0,0 +1,9 @@
+import type {SchemaTypeDefinition} from "sanity";
+import {authorType} from "./author.ts";
+import {blockContentType} from "./blockContent.ts";
+import {categoryType} from "./category.ts";
+import {postType} from "./post.ts";
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+	types: [authorType, blockContentType, categoryType, postType],
+}
