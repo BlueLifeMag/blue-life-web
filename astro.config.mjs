eebe9da35eeffdc3922d24d2c0006a35ff29bc61
Oluwasomidotun <victoroluwasomidotun@gmail.com>
2024-11-04 21:09:06 +0100

    sanity schema for author written

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
+import sanity from "@sanity/astro"
+import react from "@astrojs/react"
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		mdx(),
+		sitemap(),
+		sanity({
+			projectId: 'rl6g1f3q',
+			dataset: 'production',
+			useCdn: false,
+			studioBasePath: '/studio'
+		}),
+		react(),
+	],
 });
